ddc78ee1464eb9937c9d665836a6cf6e2717b819
CrazyPython <Jamtlu@gmail.com>
2013-01-12 20:02:16 -0500

    Update modules/calculate/line.py

--- modules/calculate/line.py
+++ modules/calculate/line.py
@@ -1,3 +1,11 @@
+from sys import version_info
+if version_info[0:1] < [2,6]:
+    import warnings
+    warnings.warn("slope as infinity will not work for this session!",Warning) #float("inf") and float("NaN") is made in 2.6
+else:
+    infinty = lambda: float("inf")
+    NaN = lambda: float("NaN")
+else:
 def negative(n):
     if n > 0:
         return abs(n)
@@ -7,13 +15,22 @@ class line(object):
     def __init__(self,a,b,grid,name,equation=None):
         self.a,self.b = a.loc,b.loc
         self.grouping = grid.group([a,b],name)
-        self.slope = (a[1]-b[1])/(a[0]-b[0])
+        try:self.slope = (a[1]-b[1])/(a[0]-b[0])
+        except ZeroDivisionError:
+            self.x_inter = a.loc[0]
+            self.y_inter = NaN()
+            self.equation = "x = {0}".format(x_inter)
+            self.slope = infinity()
+            return self
         self.equation = "y = mx+b"
         self.equation = self.equation.replace("m",str(slope))
         self.y_inter = a[1]-(a[0]*self.slope) 
         self.equation = self.equation.replace("b",str(y_inter))
         self.x_inter = negative(y_inter)/slope
     def point_on(self,point):
+        if self.slope = infinity():
+            if point.loc[0] = x_inter:
+                return True
         if point.loc[0]*self.slope)+self.y_inter == point.loc[1]:
             return True
         return False
