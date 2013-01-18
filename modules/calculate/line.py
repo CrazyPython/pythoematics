54684aadd394f69ab4bdb9caaa3b27f5d0dfd509
CrazyPython <Jamtlu@gmail.com>
2013-01-17 19:35:56 -0500

    support for slope = 0

--- modules/calculate/line.py
+++ modules/calculate/line.py
@@ -1,7 +1,7 @@
 from sys import version_info
 if version_info[0:1] < [2,6]:
     import warnings
-    warnings.warn("slope as infinity will not work for this session!",Warning) #float("inf") and float("NaN") is made in 2.6
+    warnings.warn("slope as infinity or 0 will not work for this session!",Warning) #float("inf") and float("NaN") is made in 2.6
 else:
     infinty = lambda: float("inf")
     NaN = lambda: float("NaN")
@@ -22,15 +22,25 @@ class line(object):
             self.equation = "x = {0}".format(x_inter)
             self.slope = infinity()
             return self
+        if self.slope == 0:
+            self.y_inter = a.loc10]
+            self.x_inter = NaN()
+            self.equation = "x = {0}".format(y_inter)
+            self.slope = 0
+            return self
         self.equation = "y = mx+b"
         self.equation = self.equation.replace("m",str(slope))
         self.y_inter = a[1]-(a[0]*self.slope) 
         self.equation = self.equation.replace("b",str(y_inter))
         self.x_inter = negative(y_inter)/slope
     def point_on(self,point):
-        if self.slope = infinity():
-            if point.loc[0] = x_inter:
+        if self.slope == infinity():
+            if point.loc[0] == x_inter:
+                return True
+        elif self.slope == 0:
+            if point.loc[1] == y_inter:
                 return True
         if point.loc[0]*self.slope)+self.y_inter == point.loc[1]:
             return True
         return False
+    
